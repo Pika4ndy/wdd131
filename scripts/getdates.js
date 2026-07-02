2cd87ef9e20481361c16580b6d1240dbda5b6a7e
pika4ndy <aandrianarivo1@byupathway.edu>
2026-07-02 15:15:24 +0300

    Homepage assignment done

---
+++ scripts/getdates.js
@@ -0,0 +1,16 @@
+const today = new Date();
+const year = today.getFullYear();
+
+const birthday = new Date(2008, 9, 7);
+
+const ageContainer = document.getElementById("ageCalculation");
+const copyrightYearContainer = document.getElementById("currentyear");
+
+let currentAge = year - birthday.getFullYear();
+
+ageContainer.textContent = currentAge;
+copyrightYearContainer.textContent = year;
+
+document.getElementById("lastModified").innerHTML = `Last Modification: ${document.lastModified}`;
+
+console.log(year);
